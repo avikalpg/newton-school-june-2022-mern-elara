85e68406af942290e6a5f0e1d92690b3904d7d8e
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2022-11-24 23:19:24 +0530

    Lecture 17: Data types in JS - an introduction

---
+++ Lecture17/script.js
@@ -0,0 +1,14 @@
+let a = 1;
+console.log(typeof (a));
+
+a = "Avikalp";
+console.log(typeof (a));
+
+a = [1, 2, 3];
+console.log(typeof (a));
+
+a = false;
+console.log(typeof (a));
+
+let b = a + "hello";
+console.log(b, typeof (b));
