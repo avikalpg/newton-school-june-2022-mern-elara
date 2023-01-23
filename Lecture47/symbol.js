9b103ecfed5e55e5ea6086d244b99f0ad1ffd0f2
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-01-23 23:14:06 +0530

    Symbols: minor edit

--- Lecture47/symbol.js
+++ Lecture47/symbol.js
@@ -6,3 +6,27 @@ console.log(Symbol.for('1'))
 const mySymbol2 = Symbol('1');
 
 console.log(Symbol.keyFor(Symbol.for('ajanta')));
+
+const me = {
+	fname: "Avikalp",
+	lname: "Gupta",
+}
+
+me[Symbol("age")] = 27;
+me[Symbol.for("sex")] = "Male";
+
+console.log("me: name property", Object.getOwnPropertyNames(me));
+console.log("me: symbol property", Object.getOwnPropertySymbols(me));
+
+console.log("JSON: name property", Object.getOwnPropertyNames(JSON));
+console.log("JSON: symbol property", Object.getOwnPropertySymbols(JSON));
+
+for (const symProp of Object.getOwnPropertySymbols(JSON)) {
+	console.log(symProp.description, symProp, JSON[symProp]);
+}
+
+for (const symProp of Object.getOwnPropertySymbols(me)) {
+	console.log(symProp.description, symProp, me[symProp]);
+}
+
+console.log(JSON.stringify(me));
