eea2bfcb0023eacf9c44fda8aae63ce58c279660
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-02-06 22:07:49 +0530

    Lecture 58: created a components folder and moved the Header and Footer JSX files into it

--- Lecture56/example/src/App.js
+++ Lecture56/example/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
--- Lecture56/example/src/components/Header.jsx
+++ Lecture56/example/src/components/Header.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 class Header extends React.Component {
+	componentWillMount() {
+		console.log("Component will mount method")
+	}
+
 	render() {
 		return (
 			<div>
